692b9c45a178722953b000a8a2cd0b3e048466fa
dudasc <dudatimbe@gmail.com>
2022-06-20 18:26:25 -0300

    test: created tests

--- src/modules/reports/services/get-total-farms.service.ts
+++ src/modules/reports/services/get-total-farms.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { PrismaService } from "src/modules/prisma/services/prisma.service";
+import { PrismaService } from "../../prisma/services/prisma.service";
 
 @Injectable()
 export default class GetTotalFarmsService {
---
+++ src/modules/reports/services/get-total-farms.spec.ts
@@ -0,0 +1,28 @@
+// import { PrismaService } from "src/modules/prisma/services/prisma.service";
+import { Test } from "@nestjs/testing";
+import { PrismaService } from "../../prisma/services/prisma.service";
+import GetTotalFarmsService from "./get-total-farms.service";
+
+describe('GetTotalFarmsService', () => {
+    let getTotalFarmsService: GetTotalFarmsService;
+
+    beforeEach(async() => {
+        const moduleRef = await Test.createTestingModule({
+            providers: [GetTotalFarmsService, PrismaService]
+        }).compile();
+
+        getTotalFarmsService = moduleRef.get<GetTotalFarmsService>(GetTotalFarmsService);
+    });
+
+    describe('execute function return', () => {
+        it('should be defined', async () => {
+            expect(getTotalFarmsService).toBeDefined();
+        });
+
+        it('should return total number of farms', async () => {
+            const {total}: any = await getTotalFarmsService.execute();
+
+            expect(total).not.toBeUndefined();
+        });
+    });
+});
